3c6e7ee629cf2468e9e508318fcffccddadbdfef
rafaellafanti <rafaellafantidealmeida@gmail.com>
2026-03-23 14:52:49 -0300

    feat: criação da entidade ProjetoModel

---
+++ gerenciamento_projetos/src/main/java/com/example/gerenciamento_projetos/models/ProjetoModel.java
@@ -0,0 +1,54 @@
+package com.example.gerenciamento_projetos.models;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
+public class ProjetoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_projeto")
+    private String nome;
+
+    @Column(name = "data_inicio")
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    private LocalDate dataFim;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
